06e52bba732711836522f8619ab2f01b4683c731
aritaku <aritaku19@gmail.com>
2016-06-16 17:21:55 +0900

    [add] JSQMessageViewController & 基本機能

--- meditalk/ViewController.swift
+++ meditalk/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
+import JSQMessagesViewController
 
-class ViewController: UIViewController {
+class ViewController: JSQMessagesViewController {
+    
+    var messages: [JSQMessage]?
+    var incomingBubble: JSQMessagesBubbleImage!
+    var outgoingBubble: JSQMessagesBubbleImage!
+    var incomingAvatar: JSQMessagesAvatarImage!
+    var outgoingAvatar: JSQMessagesAvatarImage!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.senderId = "user1"
+        self.senderDisplayName = "aritaku"
+        
+        let bubbleFuctory = JSQMessagesBubbleImageFactory()
+        self.incomingBubble = bubbleFuctory.incomingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleLightGrayColor())
+        self.outgoingBubble = bubbleFuctory.outgoingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleGreenColor())
+        
+        self.incomingAvatar = JSQMessagesAvatarImageFactory.avatarImageWithImage(UIImage(named: "cat")!, diameter: 64)
+        self.outgoingAvatar = JSQMessagesAvatarImageFactory.avatarImageWithImage(UIImage(named: "aritaku")!, diameter: 64)
+        
+        self.messages = []
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func didPressSendButton(button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: NSDate!) {
+        let message = JSQMessage(senderId: senderId, displayName: senderDisplayName, text: text)
+        self.messages?.append(message)
+        
+        self.finishReceivingMessageAnimated(true)
+        
+        self.receiveAutoMessage()
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        return self.messages?[indexPath.item]
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = self.messages?[indexPath.item]
+        if message?.senderId == self.senderId {
+            return self.outgoingBubble
+        }
+        return self.incomingBubble
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
+        let message = self.messages?[indexPath.item]
+        if message?.senderId == self.senderId {
+            return self.outgoingAvatar
+        }
+        return self.incomingAvatar
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (self.messages?.count)!
+    }
+    
+    func receiveAutoMessage() {
+        NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.didFinishMessageTimer(_:)), userInfo: nil, repeats: false)
+    }
+    
+    func didFinishMessageTimer(sender: NSTimer) {
+        let message = JSQMessage(senderId: "user2", displayName: "catBot", text: "にゃー")
+        self.messages?.append(message)
+        self.finishReceivingMessageAnimated(true)
+    }
 }
-
