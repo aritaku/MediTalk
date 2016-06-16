6db1c072d2052cd50b9aec34cbf2e9d38e8c1051
aritaku <aritaku19@gmail.com>
2016-06-16 11:31:26 +0900

    [add]project files

---
+++ meditalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meditalk
+//
+//  Created by 有村琢磨 on 2016/06/16.
+//  Copyright © 2016年 有村琢磨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
