5b32724be1727e8c1d4791eb73719f7b867f03e3
aritaku <aritaku19@gmail.com>
2016-06-17 20:11:37 +0900

    [add] chat_model classes

---
+++ meditalk/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  meditalk
+//
+//  Created by 有村琢磨 on 2016/06/17.
+//  Copyright © 2016年 有村琢磨. All rights reserved.
+//
+
+import RealmSwift
+
+class User: Object {
+    
+    dynamic var name = ""
+    dynamic var age = Int = 0
+    dynamic var picture: NSData = nil
+}
